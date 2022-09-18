0c9799772ca7bba9e7bf3078d3a69227e5fe774b
123name <123@hotmail.com>
2022-09-19 11:02:47 +1200

    adding react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 
+import react from '@astrojs/react';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
   output: "static",
   integrations: [
+    react()
   ],
 
   vite: {
---
+++ src/components/JobsheetTable.jsx
@@ -0,0 +1,9 @@
+ import {
+   useQuery,
+   useMutation,
+   useQueryClient,
+   QueryClient,
+   QueryClientProvider,
+ } from 'react-query'
+
+
