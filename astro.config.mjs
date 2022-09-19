39e7fda519eb2c9885db1d2c398a75d35616908f
123name <123@hotmail.com>
2022-09-19 16:47:13 +1200

    added import statement to index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  output: "static",
+  output: "server",
+  adapter: node(),
   integrations: [
     react()
   ],
