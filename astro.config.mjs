065befc395f9f920e6940c4d6de6e0eebafa0654
123name <123@hotmail.com>
2022-09-21 10:57:51 +1200

    implemented working solution using manual DOM manipulation and fetch calls on update/delete, each delete triggers an update of the server followed by a refetch to get the new server state and then the DOM is modified to match

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import node from '@astrojs/node';
+//import react from '@astrojs/react';
+//import node from '@astrojs/node';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  output: "server",
-  adapter: node(),
+  output: "static",
+ // adapter: node(),
   integrations: [
-    react()
+//    react()
   ],
 
   vite: {
--- src/config.mjs
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  // your configuration options here...
-})
